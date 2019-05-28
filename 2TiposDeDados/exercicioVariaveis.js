924c0509200f8b938309ad9332f551b626fe3d78
Rodolfo Luiz Soares dos Santos <rodolfo.luiz@rjo01.local>
2019-05-28 15:41:07 -0300

    Tipos de dados (Template String, uso de typeof)

---
+++ 2TiposDeDados/exercicioVariaveis.js
@@ -0,0 +1,20 @@
+//Declare duas variaveis uma com seu nome e outra com seu sobrenome
+
+var nome = 'Rodolfo';
+var sobrenome = 'Luiz';
+var nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(nomeCompleto);
+
+
+//  Declare uma variavel com a frase It's time
+
+var frase = `It's time`;
+var frase2 = "It's time";
+var frase3 = 'It\'s time';
+
+console.log(frase, frase2, frase3);
+
+//verifique o tipo da variável que contem seu nome
+
+console.log(typeof nomeCompleto);
---
+++ 2TiposDeDados/script.js
@@ -0,0 +1,6 @@
+// Template String
+
+var gols = 1000;
+var frase = `Romário fez ${gols * 2} gols` ;
+
+console.log(frase);
