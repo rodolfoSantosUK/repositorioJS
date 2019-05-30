c96fcb6317d8ac0dff9f2c1d604e3edfd534be03
Rodolfo Luiz Soares dos Santos <rodolfo.luiz@rjo01.local>
2019-05-30 16:08:13 -0300

    Boolean e condicionais

---
+++ 4Boolean e Condicionais/script.js
@@ -0,0 +1,15 @@
+
+
+var exp = 2e10;
+var modulo = 14 % 5;
+
+console.log(exp);
+
+console.log(modulo);
+
+var testeNan = '100' / 5;
+console.log(isNaN(testeNan));
+
+var x = '100'/ 2;
+
+console.log(x);
---
+++ 5Boolean e Condicionais/exercicio.js
@@ -0,0 +1,20 @@
+// Verifique se as seguinte variáveis são Truthy ou Falsy
+
+var nome = 'Andre';       // Truthy
+var idade = 28;           // Truthy 
+var possuiDoutorado = false;    //  Falsy
+var empregoFuturo;        // Falsy
+var dinheiroNaConta = 0;  // Falsy
+
+console.log(dinheiroNaConta);
+
+
+ 
+
+// O que irá aparecer no console ?
+
+if(('Gato' === 'gato')  && (5 > 2) ) {
+    console.log('Verdadeiro');
+}else {
+    console.log('Falso');
+}
