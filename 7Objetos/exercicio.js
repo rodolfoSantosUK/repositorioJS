ebf3dd0127a703a432e32d76befac30eb02c3b2b
Rodolfo Luiz Soares dos Santos <rodolfo.luiz@rjo01.local>
2019-06-13 18:18:25 -0300

    Trabalhando com objetos javascript

--- 7Objetos/exercicio.js
+++ 7Objetos/exercicio.js
@@ -1,17 +1,16 @@
-// Crie um objeto que represente um labrador, preto com 10 anos, que late ao ver um homem 
+// Crie um objeto com os seus dados pessoais
 
+var dadosPessoais = {
 
-var cachorro = {
-
-   raca: "labrador",
-   idade :10,
-   latir(pessoa) {
-
-   if(pessoa === 'homem') {
-   return "latir";
-  }
+  nome : 'Rodolfo',
+  sobrenome: 'Santos',
+  idade: 33 ,
+  cidade : 'Rio de Janeiro'
 
 }
+ 
+// Crie um metodo no objeto anterior, que mostre o seu nome completo
 
-
+dadosPessoais.nomeCompleto = function() {
+  return `${nome} ${sobrenome}`;
 }
